856838a81b731c6be5c1bfcdbf84aa11ce1fc0c5
michelle-yiu <yiumichelle@yahoo.com.hk>
2025-04-15 22:47:11 -0400

    Final update on Assignment 6
    added data population for NavBar & Tickets section

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-// Populate data for Overview section: 
+// 1/ Populate data for Overview section: 
 async function getOverviewData() {
     const fetchData = await fetch("../data/data.json")
     const result = await fetchData.json();
@@ -8,30 +8,32 @@ async function getOverviewData() {
 
     const keys = Object.keys(overviewData).filter(key => key.endsWith("_text"));
 
-    keys.forEach ((textKey, index) => {
+    keys.forEach((textKey, index) => {
         const valueKey = textKey.replace("_text", "_value");
 
-        const colDiv = document.createElement("div"); 
-        colDiv.classList.add("col-md-3"); 
+        const colDiv = document.createElement("div");
+        colDiv.classList.add("col-md-3");
 
         const statusBoxDiv = document.createElement("div");
-        statusBoxDiv.classList.add("status-box"); 
-        
+        statusBoxDiv.classList.add("status-box");
+
         if (textKey === "overdue_text") {
-            statusBoxDiv.classList.add("active");}
+            statusBoxDiv.classList.add("active");
+        }
 
         statusBoxDiv.innerHTML = `
         <div class="title">${overviewData[textKey]}</div>
         <div class="number">${overviewData[valueKey]}</div>`;
 
-        colDiv.appendChild(statusBoxDiv); 
+        colDiv.appendChild(statusBoxDiv);
         overviewContainer.appendChild(colDiv);
-})}
+    })
+}
 
 getOverviewData();
 
 
-// Populate data for Trends section: 
+// 2/ Populate data for Trends section: 
 async function getTrendsData() {
     const fetchData = await fetch("../data/data.json")
     const result = await fetchData.json();
@@ -41,72 +43,84 @@ async function getTrendsData() {
 
     const TrendsKeys = Object.keys(trendsData).filter(key => key.endsWith("_text"));
 
-    TrendsKeys.forEach ((textKey) => {
+    TrendsKeys.forEach((textKey) => {
         const valueKey = textKey.replace("_text", "_value");
 
-        const summaryItemDiv = document.createElement("div"); 
-        summaryItemDiv.classList.add("summary-item"); 
+        const summaryItemDiv = document.createElement("div");
+        summaryItemDiv.classList.add("summary-item");
 
         const trendsLabel = document.createElement("label");
-        trendsLabel.classList.add("title"); 
-        trendsLabel.textContent = trendsData[textKey]
+        trendsLabel.classList.add("title");
+        trendsLabel.textContent = trendsData[textKey];
 
         const trendsNum = document.createElement("text");
-        trendsNum.classList.add("number"); 
-        trendsNum.textContent = trendsData[valueKey]
+        trendsNum.classList.add("number");
+        trendsNum.textContent = trendsData[valueKey];
 
         summaryItemDiv.appendChild(trendsLabel);
-        summaryItemDiv.appendChild(trendsNum); 
+        summaryItemDiv.appendChild(trendsNum);
         trendsContainer.appendChild(summaryItemDiv);
-})}
+    })
+}
 
 getTrendsData();
 
 
+// 3/ Populate data for Tickets section: 
+async function getTicketsData() {
+    const fetchData = await fetch("../data/data.json")
+    const result = await fetchData.json();
 
-//     // nav bar part:
-//     const navBarData = result.NavBar; 
-//     const navHolder = document.getElementById("mainNav");
-//     const navLinks = navHolder.querySelectorAll(".nav-item"); 
-    
-//    const navValues = Object.values(navBarData); 
+    const ticketsData = result.UnresolvedTickets;
+    const ticketsContainer = document.getElementById('TicketsCard');
 
-//    navLinks.forEach((listItem, index) => {
-//     const span = listItem.querySelector(".nav-link-text");
-//     if (span && navValues[index]) {
-//         span.textContent = navValues[index];
-//     } 
-//    });
+    const TicketsKeys = Object.keys(ticketsData).filter(key => key.endsWith("_text"));
+
+    TicketsKeys.forEach((textKey) => {
+        const valueKey = textKey.replace("_text", "_value");
+
+        const ticketsItemDiv = document.createElement("div");
+        ticketsItemDiv.classList.add(
+            "custom-card-info-item",
+            "w-100",
+            "py-3",
+            "d-flex",
+            "flex-row",
+            "justify-content-between"
+        );
+
+        const ticketsText = document.createElement("div");
+        ticketsText.textContent = ticketsData[textKey];
+
+        const ticketsNum = document.createElement("div");
+        ticketsNum.classList.add("muted-text");
+        ticketsNum.textContent = ticketsData[valueKey]
+
+        ticketsItemDiv.appendChild(ticketsText);
+        ticketsItemDiv.appendChild(ticketsNum);
+        ticketsContainer.appendChild(ticketsItemDiv);
+    })
+}
+
+getTicketsData();
 
 
+// 4/ Populate data for NavBar: 
+async function getNavData() {
+    const fetchData = await fetch("../data/data.json")
+    const result = await fetchData.json();
 
-    // const statusItems = [
-    //     {textKey: "unresolved_text", valueKey: "unresolved_value"}, 
-    //     {textKey: "overdue_text", valueKey: "overdue_value", Active: true}, 
-    //     {textKey: "open_text", valueKey: "open_value"}, 
-    //     {textKey: "onHold_text", valueKey: "onHold_value"}, 
-    // ];
+    const NavData = result.NavBar;
+    const NavContainer = document.getElementById('mainNav');
 
-    // statusItems.forEach(item => {
-    //     const colDiv = document.createElement("div"); 
-    //     colDiv.classList.add("col-md-3"); 
+    const NavItems = NavContainer.querySelectorAll(".nav-item");
 
-    //     const statusBoxDiv = document.createElement("div");
-    //     statusBoxDiv.classList.add("status-box"); 
-    //     if (item.Active) {
-    //         statusBoxDiv.classList.add("active");
-    //     }
+    Object.keys(NavData).forEach((key, index) => {
+        const NavText = NavData[key];
+        const navLinkText = NavItems[index].querySelector(".nav-text");
+        navLinkText.textContent = NavText;
+    })
+}
 
-    //     const titleDiv = document.createElement("div"); 
-    //     titleDiv.classList.add("title");
-    //     titleDiv.textContent = overviewData[item.textKey];
+getNavData();
 
-    //     const numberDiv = document.createElement("div"); 
-    //     numberDiv.classList.add("number");
-    //     numberDiv.textContent = overviewData[item.valueKey];
-
-    //     statusBoxDiv.appendChild(titleDiv);
-    //     statusBoxDiv.appendChild(numberDiv);
-    //     colDiv.appendChild(statusBoxDiv);
-    //     overviewContainer.appendChild(colDiv);
-    // })
